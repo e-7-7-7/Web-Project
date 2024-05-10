c9003ffad6791682b217cac96754cd3ca9a3607b
Polar <108890442+o-qutb@users.noreply.github.com>
2024-05-10 22:33:54 +0300

    Modifaction to page to shows the stats

--- readradar_p2/app/page.jsx
+++ readradar_p2/app/page.jsx
@@ -1,7 +1,16 @@
-import Chart from "./Barchart";
-
-const MyPage = () => {
-  return <div></div>;
-};
-
-export default MyPage;
+import TotalRevenuePerMonth from "@/app/charts/TotalRevenuePerMonth";
+import AveragePurchaseAmountPerBuyer from "@/app/charts/AveragePurchaseAmountPerBuyer";
+import TopBooksBySales from "@/app/charts/TopBooksBySales";
+import AverageQuantitySoldPerBook from "@/app/charts/AverageQuantitySoldPerBook";
+import CountTransactionsPerSeller from "@/app/charts/countTransactionsPerSeller";
+export default async function home() {
+  return (
+    <div>
+      <TopBooksBySales />
+      <AverageQuantitySoldPerBook />
+      <AveragePurchaseAmountPerBuyer />
+      <CountTransactionsPerSeller />
+      <TotalRevenuePerMonth />
+    </div>
+  );
+}
